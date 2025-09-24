7f4cad58bf23b0a2f4eac735a0d6cd1d245f859b
yohagos <yosef.hagos@googlemail.com>
2025-09-24 20:07:34 +0200

    first micro frontend component - employee-add

---
+++ projects/mfe-employee-add/federation.config.js
@@ -0,0 +1,38 @@
+const { withNativeFederation, shareAll } = require('@angular-architects/native-federation/config');
+
+module.exports = withNativeFederation({
+
+  name: 'mfe-employee-add',
+
+  exposes: {
+    './Component': './projects/mfe-employee-add/src/app/app.ts',
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    '@personal-manager/shared-lib': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto'
+    }
+  },
+
+  skip: [
+    'rxjs/ajax',
+    'rxjs/fetch',
+    'rxjs/testing',
+    'rxjs/webSocket',
+    // Add further packages you don't need at runtime
+  ],
+
+  // Please read our FAQ about sharing libs:
+  // https://shorturl.at/jmzH0
+
+  features: {
+    // New feature for more performance and avoiding
+    // issues with node libs. Comment this out to
+    // get the traditional behavior:
+    ignoreUnusedDeps: true
+  }
+
+});
---
+++ projects/mfe-employee-add/src/app/app.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from "@angular/forms";
+import { Component, inject, signal } from '@angular/core';
+import { EmployeeService } from './../../../shared-lib/src/lib/employee-service';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    FormsModule,
+  ],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  firstName = signal("")
+  lastName = signal("")
+  email = signal("")
+
+  private employeesService = inject(EmployeeService)
+
+  add(form: NgForm) {
+    this.employeesService.addEmployee({
+      id: 0,
+      firstName: this.firstName(),
+      lastName: this.lastName(),
+      email: this.email(),
+      activate: true,
+    })
+    this.clearFormContent(form)
+  }
+
+  clearFormContent(form: NgForm) {
+    this.firstName.set('')
+    this.firstName.set('')
+    this.firstName.set('')
+    form.resetForm()
+  }
+
+}
