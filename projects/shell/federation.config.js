053775f55a8ecc72fb8c12326b98c0a0b45595a1
yohagos <yosef.hagos@googlemail.com>
2025-09-25 17:27:09 +0200

    adjusting shell component

--- projects/shell/federation.config.js
+++ projects/shell/federation.config.js
@@ -2,6 +2,11 @@ const { withNativeFederation, shareAll } = require('@angular-architects/native-f
 
 module.exports = withNativeFederation({
   name: 'shell',
+  remotes: {
+    'mfe-employee-list': 'http://localhost:4201/remoteEntry.json',
+    'mfe-employee-add': 'http://localhost:4202/remoteEntry.json',
+    'mfe-employee-deactivate': 'http://localhost:4203/remoteEntry.json',
+  },
   shared: {
     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
     '@personal-manager/shared-lib': {
