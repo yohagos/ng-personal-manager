de189c4c4745e6853718617f29ee0264b4358b84
yohagos <yosef.hagos@googlemail.com>
2025-10-22 16:44:53 +0200

    updating mfe's list with icons, angular material and connecting to navigation service

--- projects/mfe-employee-list/src/app/app.ts
+++ projects/mfe-employee-list/src/app/app.ts
@@ -1,19 +1,31 @@
 import { Component, computed, inject } from '@angular/core';
-import { EmployeeService } from '@personal-manager/shared-lib';
+import { EmployeeService, NavigationService } from '@personal-manager/shared-lib';
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   private employeeService = inject(EmployeeService)
+  private _navigationService = inject(NavigationService)
 
   employees = computed(() => this.employeeService.get_employees())
 
   deactivateEmployeeById(id: number) {
     this.employeeService.deactivateEmployee(id)
     this.employees = computed(() => this.employeeService.get_employees())
+  }
+
+  openDetailsById(id: number) {
+    console.log("open details by employee id => ", id)
+    this._navigationService.navigateToEmployeeDetailById(id)
   }
 }
